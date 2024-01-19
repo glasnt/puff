cbb784e22a2ea2601d104f3bb777ea5e2e26d0f8
Katie McLaughlin <katie@glasnt.com>
2024-01-19 14:58:31 +1100

    latest latest

--- build_database.py
+++ build_database.py
@@ -42,7 +42,7 @@ def build_database(repo_path):
         title = fp.readline().lstrip("#").strip()
         body = fp.read().strip()
         path = str(filepath.relative_to(root))
-        url = "https://github.com/glasnt/puff/blob/main/{}".format(path)
+        url = "https://github.com/glasnt/puff/blob/latest/{}".format(path)
         record = {
             "path": path.replace("/", "_"),
             "topic": path.split("/")[0],
