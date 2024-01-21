be4345194716be5f40dd6b6dd7927d3b0d602491
Katie McLaughlin <katie@glasnt.com>
2024-01-22 08:31:15 +1100

    docs

--- update_readme.py
+++ update_readme.py
@@ -8,15 +8,18 @@ root = pathlib.Path(__file__).parent.resolve()
 index_re = re.compile(r"<!\-\- index starts \-\->.*<!\-\- index ends \-\->", re.DOTALL)
 
 if __name__ == "__main__":
+
+    # load topics
     db = sqlite_utils.Database(root / "til.db")
     by_topic = {}
     for row in db["til"].rows_where(order_by="created_utc"):
         by_topic.setdefault(row["topic"], []).append(row)
     index = ["<!-- index starts -->"]
 
-    # Alphabetize the topics
+    # alphabetize the topics
     topics = list(by_topic.keys())
 
+    # write topics to list
     for topic in topics:
         index.append("## {}\n".format(topic))
         rows = by_topic[topic]
@@ -30,10 +33,14 @@ if __name__ == "__main__":
     if index[-1] == "":
         index.pop()
     index.append("<!-- index ends -->")
+
+    
     if "--rewrite" in sys.argv:
+        # write results
         readme = root / "README.md"
         index_txt = "\n".join(index).strip()
         readme_contents = readme.open().read()
         readme.open("w").write(index_re.sub(index_txt, readme_contents))
     else:
+        # preview results
         print("\n".join(index))
